9f86ea8f71df346c808543072e04126541a310dc
MohamedAllam <mohamedmamdouh392000@gmail.com>
2024-04-11 10:17:15 +0200

    Adding Drink Controller

--- DrinkAndGo/Startup.cs
+++ DrinkAndGo/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.Data.Mocks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,9 @@ namespace DrinkAndGo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IDrinkRepository, MockDrinkRepository>();
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
